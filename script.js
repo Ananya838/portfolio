b52d9566a936fe60077081794690591f90c8ddd0
Ananya838 <aniananya586@gmail.com>
2025-11-17 23:37:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ function showContent() {
         }
     });
 }
+function toggleMenu() {
+    document.getElementById("navLinks").classList.toggle("show");
+}
+
+function closeMenu() {
+    document.getElementById("navLinks").classList.remove("show");
+}
+
